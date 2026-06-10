b8df308c0ad5c032c73189a0a200839eed2e9e31
Aurore Langelier <aurore@alangelier.com>
2026-06-10 23:52:31 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,6 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     'cookie-universal-nuxt',
-    '@vercel/speed-insights',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
