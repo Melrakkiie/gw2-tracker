c7e7f013599213dff3e585f93d57521741621337
Aurore Langelier <aurore@alangelier.com>
2026-06-19 01:27:26 +0200

    better conditional rendering for mastery icons + added future proof icon for ongoing expansions

--- components/masteryIcon.vue
+++ components/masteryIcon.vue
@@ -1,45 +1,46 @@
 <template>
   <span>
     <img
-      v-if="region === 'Heart of Thorns' || region === 'Maguuma'"
-      src="~/static/mp_hot.png"
-      :height="height"
-    />
-    <img
       v-if="region === 'Central Tyria' || region === 'Tyria'"
       src="~/static/mp_tyria.png"
       :height="height"
     />
     <img
-      v-if="region === 'Path of Fire' || region === 'Desert'"
+      v-else-if="region === 'Heart of Thorns' || region === 'Maguuma'"
+      src="~/static/mp_hot.png"
+      :height="height"
+    />
+    <img
+      v-else-if="region === 'Path of Fire' || region === 'Desert'"
       src="~/static/mp_pof.png"
       :height="height"
     />
     <img
-      v-if="region === 'Icebrood Saga' || region === 'Tundra'"
+      v-else-if="region === 'Icebrood Saga' || region === 'Tundra'"
       src="~/static/mp_is.png"
       :height="height"
     />
     <img
-      v-if="region === 'End of Dragons' || region === 'Jade'"
+      v-else-if="region === 'End of Dragons' || region === 'Jade'"
       src="~/static/mp_eof.png"
       :height="height"
     />
     <img
-      v-if="region === 'Secrets of the Obscure' || region === 'Sky'"
+      v-else-if="region === 'Secrets of the Obscure' || region === 'Sky'"
       src="~/static/mp_soto.png"
       :height="height"
     />
     <img
-      v-if="region === 'Janthir Wilds' || region === 'Wild'"
+      v-else-if="region === 'Janthir Wilds' || region === 'Wild'"
       src="~/static/mp_jw.png"
       :height="height"
     />
     <img
-      v-if="region === 'Visions of Eternity' || region === 'Magic'"
+      v-else-if="region === 'Visions of Eternity' || region === 'Magic'"
       src="~/static/mp_voe.png"
       :height="height"
     />
+    <img v-else src="~/static/construction.png" :height="height" />
   </span>
 </template>
 
