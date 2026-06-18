56c62c2eada2967ace7666201f2062de95786854
Aurore Langelier <aurore@alangelier.com>
2026-06-19 01:14:23 +0200

    Fix masteries api region name breaking change + add masteries after Icebrood Saga

--- components/masteryIcon.vue
+++ components/masteryIcon.vue
@@ -1,21 +1,45 @@
 <template>
   <span>
     <img
-      v-if="region === 'Maguuma'"
+      v-if="region === 'Heart of Thorns' || region === 'Maguuma'"
       src="~/static/mp_hot.png"
       :height="height"
     />
     <img
-      v-if="region === 'Tyria'"
+      v-if="region === 'Central Tyria' || region === 'Tyria'"
       src="~/static/mp_tyria.png"
       :height="height"
     />
     <img
-      v-if="region === 'Desert'"
+      v-if="region === 'Path of Fire' || region === 'Desert'"
       src="~/static/mp_pof.png"
       :height="height"
     />
-    <img v-if="region === 'Tundra'" src="~/static/mp_is.png" :height="height" />
+    <img
+      v-if="region === 'Icebrood Saga' || region === 'Tundra'"
+      src="~/static/mp_is.png"
+      :height="height"
+    />
+    <img
+      v-if="region === 'End of Dragons' || region === 'Jade'"
+      src="~/static/mp_eof.png"
+      :height="height"
+    />
+    <img
+      v-if="region === 'Secrets of the Obscure' || region === 'Sky'"
+      src="~/static/mp_soto.png"
+      :height="height"
+    />
+    <img
+      v-if="region === 'Janthir Wilds' || region === 'Wild'"
+      src="~/static/mp_jw.png"
+      :height="height"
+    />
+    <img
+      v-if="region === 'Visions of Eternity' || region === 'Magic'"
+      src="~/static/mp_voe.png"
+      :height="height"
+    />
   </span>
 </template>
 
@@ -25,7 +49,7 @@ export default {
   props: {
     region: {
       type: String,
-      default: 'Tyria',
+      default: 'Central Tyria',
     },
     height: {
       type: Number,
